5a52e399e185b5b712179becfd14ec29c0e857af
Adriel Arsenault <adriel.arsenault@gmail.com>
2017-09-17 17:02:32 -0400

    introduced proptypes

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
+// import { shape, string } from 'prop-types';
+import ShowCard from './ShowCard';
+import preload from '../data.json';
 
-const Search = () => <h1>hi lol this is the search</h1>;
+const Search = () => <div className="search">{preload.shows.map(show => <ShowCard show={show} />)}</div>;
+
+// ShowCard.propTypes = {
+//     show: shape({
+//         poster: string.isRequired,
+//         title: string.isRequired,
+//         year: string.isRequired,
+//         description: string.isRequired
+//     }).isRequired
+// };
 
 export default Search;
+
+/*
+
+
+Notes:
+
+To simply look at the data dump:
+<pre>
+    <code>{JSON.stringify(preload, null, 4)} </code>
+</pre>
+
+
+*/
