57896e840dcbbf0588f72005965de6c1dfde6ddd
Adriel Arsenault <adriel.arsenault@gmail.com>
2017-09-18 23:38:52 -0400

    managing state

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import { shape, string } from 'prop-types';
 import ShowCard from './ShowCard';
 import preload from '../data.json';
 
-const Search = () => (
-    <div className="search">
-        <div>
-            {preload.shows.map(show => <ShowCard key={show.imdbID} show={show} />)}
-        </div>
-    </div>
-    );
+class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            serchTerm: 'this is some sort of debug statement'
+        };
+
+        // bind this in the constructor so it gets called only once
+        this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+    }
+
+    handleSearchTermChange(event) {
+        this.setState({ searchTerm: event.target.value });
+    }
+
+    render() {
+        return (
+            <div className="search">
+                <header>
+                    <h1>svideo</h1>
+
+                    <input
+                        onChange={this.handleSearchTermChange}
+                        value={this.state.searchTerm}
+                        type="text"
+                        placeholder="Search"
+                    />
+                </header>
+                <div> {preload.shows.map(show => <ShowCard key={show.imdbID} show={show} />)}</div>
+            </div>
+        );
+    }
+}
 
 // ShowCard.propTypes = {
 //     show: shape({
